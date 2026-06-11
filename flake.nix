f9c9f947ed023acac808c3259c842840e25ec952
cbass-d <sebassd1.12@gmail.com>
2026-06-10 23:08:25 -0500

    updated flake

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
 	description = "bpfimp dev environment";
 
 	inputs = {
-		nixpgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
 		rust-overlay = {
-			url = "github:oxicalla/rust-overlay";
+			url = "github:oxalica/rust-overlay";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
@@ -17,9 +17,28 @@
 				inherit system;
 				overlays = [ rust-overlay.overlays.default ];
 			};
+			rust-toolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+				toolchain.default.override {
+					extensions = [ "rust-src" ];
+				});
+			rustupShim = pkgs.writeShellScriptBin "rustup" ''
+				if [ "$1" = "run" ]; then
+					shift 2
+				fi
+				exec "$@"
+			'';
+
+			bpf-linker = pkgs.bpf-linker.override {
+				llvmPackagesForLinker = pkgs.llvmPackages_22;
+			};
 		in
 		{
 			devShells.${system}.default = pkgs.mkShell {
+				packages = [
+					rustupShim
+					rust-toolchain
+					bpf-linker
+				];
 			};
 		};
 }
